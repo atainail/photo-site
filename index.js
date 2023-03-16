b102c67ee4b932bdabfaf37b9a89a9be4c6630d5
Atai Nail <81722547+atainail@users.noreply.github.com>
2023-03-15 23:20:24 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -14,17 +14,28 @@ $('.navigation-import').load("nav.txt");
 
 
 //imports and puts all photos from folder into an array 
-
 let weddingPicsArr = [];
+let headShotsArr = [];
 
-function importPictures(totalNumOfPhotos, arrName, folderName, photoType){
-    for(let i = 1; i < totalNumOfPhotos + 1; i++){
-        arrName[i] = `<img class="reveal" src="${folderName}/${photoType}${i}.jpg">`;
-        document.querySelector(".wedding-photos").innerHTML +=  `<img class="reveal" src="${folderName}/${photoType}${i}.jpg">`;   
-         
+function importPictures(totalNumOfPhotos, arrName, folderName, photoType) {
+    for (let i = 1; i < totalNumOfPhotos + 1; i++) {
+
+        arrName[i] = `<img class="reveal " src="${folderName}/${photoType}${i}.jpg">`;
+        document.querySelector('.wedding-photos').innerHTML += `<img class="reveal " src="${folderName}/${photoType}${i}.jpg">`;
+        document.querySelector('.head-shots').innerHTML += `<img class="reveal " src="${folderName}/${photoType}${i}.jpg">`;
     }
+
 }
-importPictures(17, weddingPicsArr, 'wedding-pics', 'wedding');
+
+let title = $(document).find("title").text();
+
+if (title.includes('Weddings')) {
+    importPictures(17, weddingPicsArr, 'wedding-pics', 'wedding');
+}
+else if (title.includes('Headshots')) {
+    importPictures(7, headShotsArr, 'headshots', 'head');
+}
+
 
 
 
@@ -45,11 +56,32 @@ function reveal() {
 
 
 
-  window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    if(scroll + 1 > scroll || scroll - 1 < scroll){
-        document.querySelector(".scroll-down").innerHTML = "";
-    } 
+ //displays the scroll down arrow 
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  if (scroll + 1 > scroll || scroll - 1 < scroll) {
+      document.querySelector(".scroll-down").innerHTML = "";
+  }
 });
+
+
+//displays the arrow to skip back to the top of the page
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  let pageHeight = document.body.scrollHeight;
+  if (scroll < pageHeight/6) {
+      document.querySelector(".top-of-page").innerHTML = ""; 
+  } else if (scroll > pageHeight/6){
+      document.querySelector(".top-of-page").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">'
+    +  '<path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>'
+    + '</svg>'
+  }
+});
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 
 
