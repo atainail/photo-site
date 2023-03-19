c4d2671fcd2e6cb58113cbd991d77af695f7b170
Atai Nail <81722547+atainail@users.noreply.github.com>
2023-03-18 20:52:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -18,13 +18,16 @@ $('.dropdown').hover(function(){
 //imports and puts all photos from folder into an array 
 let weddingPicsArr = [];
 let headShotsArr = [];
+let natureArr = [];
 
 function importPictures(totalNumOfPhotos, arrName, folderName, photoType) {
     for (let i = 1; i < totalNumOfPhotos + 1; i++) {
 
         arrName[i] = `<img class="reveal " src="${folderName}/${photoType}${i}.jpg">`;
-        document.querySelector('.wedding-photos').innerHTML += `<img class="reveal " src="${folderName}/${photoType}${i}.jpg">`;
-        document.querySelector('.head-shots').innerHTML += `<img class="reveal " src="${folderName}/${photoType}${i}.jpg">`;
+        document.querySelector('.wedding-photos').innerHTML += `<img class="reveal " src="${folderName}/${photoType} (${i}).jpg">`;
+        document.querySelector('.head-shots').innerHTML += `<img class="reveal " src="${folderName}/${photoType} (${i}).jpg">`;
+        document.querySelector('.nature-shots').innerHTML += `<img class="reveal " src="${folderName}/${photoType} (${i}).jpg">`;
+        
     }
 
 }
@@ -32,12 +35,17 @@ function importPictures(totalNumOfPhotos, arrName, folderName, photoType) {
 let title = $(document).find("title").text();
 
 if (title.includes('Weddings')) {
-    importPictures(17, weddingPicsArr, 'wedding-pics', 'wedding');
+  // wedding photos from https://www.pexels.com/search/wedding/
+    importPictures(25, weddingPicsArr, 'wedding-pics', 'wedding');
 }
 else if (title.includes('Headshots')) {
-    importPictures(7, headShotsArr, 'headshots', 'head');
+  //headshot photos from https://www.pexels.com/search/headshots/
+    importPictures(35, headShotsArr, 'headShots', 'head');
 }
-
+else if (title.includes('Nature')) {
+  //headshot photos from https://www.pexels.com/search/headshots/
+    importPictures(30, natureArr, 'natureShots', 'nature');
+}
 
 
 
