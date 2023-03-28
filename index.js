c1a7529b8d09c8e9b4154fa7dca2cc2a96970865
Atai Nail <81722547+atainail@users.noreply.github.com>
2023-03-28 16:58:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -82,9 +82,11 @@ window.addEventListener("scroll", (event) => {
   if (scroll < pageHeight/6) {
       document.querySelector(".top-of-page").innerHTML = ""; 
   } else if (scroll > pageHeight/6){
-      document.querySelector(".top-of-page").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">'
+      document.querySelector(".top-of-page").innerHTML = 'Top'
+    + '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">'
     +  '<path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>'
     + '</svg>'
+    
   }
 });
 
